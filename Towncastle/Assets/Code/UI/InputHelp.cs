429ab356daa73a69d2e84c0e5875fc03251bd5e9
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2020-01-17 18:33:49 +0200

    Separated input help into tabs

---
+++ Towncastle/Assets/Code/UI/InputHelp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHelp : MonoBehaviour
+{
+#pragma warning disable 0649
+
+    [SerializeField]
+    private GameObject basicHelp;
+
+    [SerializeField]
+    private GameObject advancedHelp;
+
+#pragma warning restore 0649
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
+    private void Start()
+    {
+        ActivateBasic();
+    }
+
+    public void ActivateBasic()
+    {
+        basicHelp.gameObject.SetActive(true);
+        advancedHelp.gameObject.SetActive(false);
+    }
+
+    public void ActivateAdvanced()
+    {
+        basicHelp.gameObject.SetActive(false);
+        advancedHelp.gameObject.SetActive(true);
+    }
+}
